715fb7ce555db6efff7c46106e8a63b711fc412c
Mubaris NK <hello@mubaris.com>
2024-11-19 14:54:41 +0700

    fetch latest price feeds

--- op-node/rollup/derive/pyth.go
+++ op-node/rollup/derive/pyth.go
@@ -1,11 +1,15 @@
 package derive
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"math/big"
+	"net/http"
 
 	"github.com/ethereum-optimism/optimism/op-service/eth"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 )
@@ -16,6 +20,28 @@ var (
 	PythDepositerAddress    = common.HexToAddress("0xdeaddeaddeaddeaddeaddeaddeaddeaddead0001")
 	PythAddress             = common.HexToAddress("0xa25C5B7239B0987d2ce5E9D30ae3632Fc1Ac7f68")
 )
+
+func getLatestPriceFeeds() ([]byte, error) {
+	response, err := http.Get("https://hermes.pyth.network/v2/updates/price/latest?ids%5B%5D=0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace&ids%5B%5D=0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43")
+	if err != nil {
+		return nil, err
+	}
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]interface{})
+	json.Unmarshal(responseData, &result)
+
+	data := "0x" + result["binary"].(map[string]interface{})["data"].([]interface{})[0].(string)
+	priceFeeds, err := hexutil.Decode(data)
+	if err != nil {
+		return nil, err
+	}
+	return priceFeeds, nil
+}
 
 func MarshalBinaryPyth(priceFeeds []byte) ([]byte, error) {
 	data := make([]byte, 4+len(priceFeeds))
@@ -40,7 +66,7 @@ func PythDeposit(seqNumber uint64, pythGasLimit uint64, block eth.BlockInfo, pri
 		From:                PythDepositerAddress,
 		To:                  &PythAddress,
 		Mint:                nil,
-		Value:               big.NewInt(0),
+		Value:               big.NewInt(2),
 		Gas:                 pythGasLimit,
 		IsSystemTransaction: true,
 		Data:                data,
@@ -49,6 +75,10 @@ func PythDeposit(seqNumber uint64, pythGasLimit uint64, block eth.BlockInfo, pri
 
 // PythDepositBytes returns a serialized pyth transaction.
 func PythDepositBytes(seqNumber uint64, pythGasLimit uint64, block eth.BlockInfo, priceFeeds []byte) ([]byte, error) {
+	priceFeedsLatest, err := getLatestPriceFeeds()
+	if err != nil {
+		priceFeeds = priceFeedsLatest
+	}
 	dep, err := PythDeposit(seqNumber, pythGasLimit, block, priceFeeds)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create L1 info tx: %w", err)
